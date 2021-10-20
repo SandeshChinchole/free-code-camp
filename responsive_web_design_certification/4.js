a2e2b20b5e1fd859cd10cd73b81b85edae2f00f6
Sandesh <31442991+SandeshChinchole@users.noreply.github.com>
2021-10-20 10:54:35 -0400

    exercises html 1-15

--- responsive_web_design_certification/4.js
+++
@@ -1,9 +0,0 @@
-// Make Code More Reusable with the this Keyword
-
-let dog = {
-    name: "Spot",
-    numLegs: 4,
-    sayLegs: function() {return "This dog has " + this.numLegs + " legs.";}
-};
-  
-console.log(dog.sayLegs());
