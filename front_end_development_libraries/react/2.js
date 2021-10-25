bc717e0ea28388fbb2a14b8e8171a2d21d334d94
Sandesh <31442991+SandeshChinchole@users.noreply.github.com>
2021-10-25 11:13:35 -0400

    react 1-5

---
+++ front_end_development_libraries/react/2.js
@@ -0,0 +1,22 @@
+const JSX = <div>
+  <h1>Hello React</h1>
+  <p>Learning React is fun</p>
+  <p>Things I like about React</p>
+  <ul>
+    <li>Very popular</li>
+    <li>In demand technology</li>
+    <li>Maintained by Facebook</li>
+  </ul>
+</div>;
+
+const JSX = (
+  <div>
+    <h1>heading</h1>
+    <p>paragraph</p>
+    <ul>
+      <li>one</li>
+      <li>two</li>
+      <li>three</li>
+    </ul>
+  </div>
+);
